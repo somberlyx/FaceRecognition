c03f7589584b8a031c2c8bba4897bbd00f254264
somberlyx <lybasiddiqui02@gmail.com>
2024-08-27 14:35:13 +0400

    Place buttons on the window

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ class App:
     self.main_window.geometry("1200x520+350+100")
 
     self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login,)
+    self.login_button_main_window.place(x=750, y=300)
 
-    self.register_button_main_window = util.get_button(self.main_window, 'register new user', 'gray', self.register_new_user, fg='black')
+    self.register_button_main_window = util.get_button(self.main_window, 'register new user', 'gray', self.register, fg='black')
+    self.register_button_main_window.place(x=750, y=400)
+
+  def login(self):
+    pass
+
+  def register(self):
+    pass
 
   def start(self):
     self.main_window.mainloop()
